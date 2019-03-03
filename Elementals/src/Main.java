34ddcee691ff5e995eaa89c22c841548636271b3
Greg Dailey <greg.calway@gmail.com>
2019-03-03 11:17:50 -0800

    a

---
+++ Elementals/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	private static Main_Frame currentFrame;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		currentFrame = new Main_Frame();
+		currentFrame.start();
+		currentFrame.setDefaultCloseOperation(2);
+	}
+
+}
---
+++ Elementals/src/Main_Frame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Main_Frame extends JFrame {
+	private Main_Panel suitePanel;
+
+	public Main_Frame() {
+		suitePanel = new Main_Panel();
+		getContentPane().add(suitePanel);
+	}
+
+
+	public void start() {
+		this.setSize(700, 700);
+		this.setVisible(true);
+	}
+
+}
