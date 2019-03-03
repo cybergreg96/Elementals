783053e99e646ef930af0fd86e4898d7da620f4e
Greg Dailey <greg.calway@gmail.com>
2019-03-03 11:56:45 -0800

    Initial push. Broke Main_Panel into multiple objects/classes

--- Elementals/src/Main.java
+++ Elementals/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
 	private static Main_Frame currentFrame;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		currentFrame = new Main_Frame();
